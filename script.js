837367c30c29ea1b33c8595933f32a6c2fa7e206
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-03-09 13:48:07 -0500

    created css skeleton w grid, rendered Mew to page

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let pokeSprites = `https://raw.githubusercontent.com/PokeAPI/sprites/master/spri
 //adding this to axios.get goes wild in the console log
 
 let url = `https://pokeapi.co/api/v2/pokemon/machop`
-
+let pkmnLeft = document.querySelector('#pokesideleft')
+let pkmnRight = document.querySelector('#pokesideright')
+let pkmnBody = document.querySelector('#pkmnbody')
 
 
 
@@ -10,8 +12,20 @@ async function getPokemon() {
   try {
     const response = await axios.get(url);
     console.log(response.data);
+
+
+
   } catch (err) {
     console.log(err.message);
   }
 }
 getPokemon()
+
+function createPokeImage(pokeID){
+  let pokeImage = document.createElement('img')
+  pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+
+  pkmnRight.append(pokeImage);
+  pkmnRight.append(pokeImage);
+}
+createPokeImage()
