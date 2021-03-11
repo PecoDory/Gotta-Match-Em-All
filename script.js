3a97fee031c935dcc98a3fd28bb0174a0c2f33da
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-03-11 14:45:56 -0500

    pulling randomly from api

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 let pokeSprites = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/shiny/1.png`
 //adding this to axios.get goes wild in the console log
 
-let url = `https://pokeapi.co/api/v2/pokemon/ivysaur`
+
 let pkmnLeft = document.querySelector('#pokesideleft')
 let pkmnRight = document.querySelector('#pokesideright')
 let pkmnBody = document.querySelector('#pkmnbody')
 let numCaught = document.querySelector('#numcaught')
 let congrats = document.querySelector('#congrats')
-
-// document.addEventListener('DOMContentLoaded', () => {
+let button = document.querySelector('#buy-pokeballs')
 
   const wildPkmn = [
-    { name: 'Ditto', img: 'pokemon-pics/ditto.png' }, { name: 'Ditto', img: 'pokemon-pics/ditto.png' },
-    { name: 'Charmander', img: 'pokemon-pics/charmander.png' }, { name: 'Charmander', img: 'pokemon-pics/charmander.png' },
-    { name: 'Haunter', img: 'pokemon-pics/haunter.png' }, { name: 'Haunter', img: 'pokemon-pics/haunter.png' },
-    { name: 'Machop', img: 'pokemon-pics/machop.png' }, { name: 'Machop', img: 'pokemon-pics/machop.png' },
-    { name: 'Eevee', img: 'pokemon-pics/eevee.png' }, { name: 'Eevee', img: 'pokemon-pics/eevee.png' },
-    { name: 'Blastoise', img: 'pokemon-pics/blastoise.png' }, { name: 'Blastoise', img: 'pokemon-pics/blastoise.png' }]
+    // { name: 'Ditto', img: 'pokemon-pics/ditto.png' }, { name: 'Ditto', img: 'pokemon-pics/ditto.png' },
+    // { name: 'Charmander', img: 'pokemon-pics/charmander.png' }, { name: 'Charmander', img: 'pokemon-pics/charmander.png' },
+    // { name: 'Haunter', img: 'pokemon-pics/haunter.png' }, { name: 'Haunter', img: 'pokemon-pics/haunter.png' },
+    // { name: 'Machop', img: 'pokemon-pics/machop.png' }, { name: 'Machop', img: 'pokemon-pics/machop.png' },
+    // { name: 'Eevee', img: 'pokemon-pics/eevee.png' }, { name: 'Eevee', img: 'pokemon-pics/eevee.png' },
+    // { name: 'Blastoise', img: 'pokemon-pics/blastoise.png' }, { name: 'Blastoise', img: 'pokemon-pics/blastoise.png' }
+  ]
+
+  
+
+
+
+
+
+
+  
+
 
   
   
-
-
-  
-  wildPkmn.sort(() => 0.5 - Math.random())
   const cardGrid = document.querySelector('.cardgrid')
-
   
   
   
-  function tallGrass() {
+function tallGrass() {
+    console.log('tallGrass')
     for (let i = 0; i < wildPkmn.length; i++) {
       const card = document.createElement('img')
       card.setAttribute('src', 'pokemon-pics/grasstiletwo.png') 
@@ -40,14 +46,15 @@ let congrats = document.querySelector('#congrats')
     }
   }
   
-  tallGrass()
+  
 
 
   const pokemonCaught = []
   let pokemonFound = []
   let pokemonFoundData = []
 
-  function checkCaughtPkmn() {
+function checkCaughtPkmn() {
+    console.log('checkCaughtPkmn')
     const caughtPkmn = document.querySelectorAll('img')
     const pkmnOneData = pokemonFoundData[0]
     const pkmnTwoData = pokemonFoundData[1]
@@ -64,47 +71,84 @@ let congrats = document.querySelector('#congrats')
     }
     pokemonFound = []
     pokemonFoundData = []
-    numCaught.textContent = `You've caught ${pokemonCaught.length} Pokemon!` 
+    numCaught.textContent = `You've caught ${pokemonCaught.length} Pokémon!` 
     if (pokemonCaught.length === wildPkmn.length / 2)
       congrats.textContent = "Uh oh! All out of Pokéball's! Go Get some more!"
-    removeEventListener ("click", encounter)
+    
+    // removeEventListener ("click", encounter)
   }
     
     
     
   
-  function encounter(){
+function encounter() {
+    console.log('encounter')
     const pokemonType = this.getAttribute('data-id')
     pokemonFound.push(wildPkmn[pokemonType].name)
     pokemonFoundData.push(pokemonType)
-    this.setAttribute('src', wildPkmn[pokemonType].img)
+    this.setAttribute('src', wildPkmn[pokemonType].sprites.front_default)
     if (pokemonFound.length === 2) {
-      setTimeout(checkCaughtPkmn, 500)
+      setTimeout(checkCaughtPkmn, 200)
     }
   }
   
   
 
 
-
+let url = `https://pokeapi.co/api/v2/pokemon/`
 
 async function getPokemon() {
   try {
-    const response = await axios.get(url);
-    console.log(response.data);
+    console.log('getPokemon')
+    for (let i = 1; i <= 6; i++) {
+      console.log(`${url}${i}`)
+      //make random num
+      let randomPkmn = getRandomNumber(150)
+      console.log(randomPkmn)
+      //api call for pkmn 
+      const response = await axios.get(`${url}${randomPkmn}`);
+      console.log(response.data);
+      wildPkmn.push(response.data);
+      wildPkmn.push(response.data);
+    }
+    
+    
 
 
 
   } catch (err) {
     console.log(err.message);
   }
-}
-getPokemon()
+  console.log(wildPkmn)
 
-function createPokeImage(pokeID){
-  let pokeImage = document.createElement('img')
-  pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
-
-  pkmnRight.append(pokeImage);
 }
-createPokeImage(151)
+
+
+
+
+async function playGame() {
+  console.log('playGame')
+  await getPokemon()
+  wildPkmn.sort(() => 0.5 - Math.random())
+  tallGrass()
+  }
+playGame();
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
+// function createPokeImage(pokeID){
+//   let pokeImage = document.createElement('img')
+//   pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+
+//   pkmnRight.append(pokeImage);
+// }
+// createPokeImage(151)
+
+
+
+    
+
